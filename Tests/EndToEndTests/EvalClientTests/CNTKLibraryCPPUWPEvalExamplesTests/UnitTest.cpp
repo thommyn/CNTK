c36f2bd9108cd793d7d7bc482ad53484e6cf6f77
Artur Laksberg <arturl@microsoft.com>
2017-07-21 20:05:18 -0700

    Change UWP unit tests to consume 01_OneHidden.model from the Documents folder

--- Tests/EndToEndTests/EvalClientTests/CNTKLibraryCPPUWPEvalExamplesTests/UnitTest.cpp
+++ Tests/EndToEndTests/EvalClientTests/CNTKLibraryCPPUWPEvalExamplesTests/UnitTest.cpp
@@ -1,16 +1,57 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "CNTKLibrary.h"
+
 using namespace CNTK;
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Windows::Storage;
 
-void MultiThreadsEvaluation(bool);
+void MultiThreadsEvaluation(const wchar_t* modelFileName, bool);
 
 namespace UWPEvalTests
 {
     TEST_CLASS(TestClass)
     {
+    private:
+        concurrency::task<Platform::String^> GetModelFilePath(Platform::String^ modelFileName)
+        {
+            auto modelFileOp = KnownFolders::DocumentsLibrary->GetFileAsync(modelFileName);
+            return concurrency::create_task(modelFileOp).then([modelFileName](concurrency::task<StorageFile^> modelFileTask) {
+                try
+                {
+                    // The file cannot be read directly from the DocumentsLibrary, so copy the file into the local app folder
+                    auto modelFile = modelFileTask.get();
+                    auto localFolder = Windows::Storage::ApplicationData::Current->LocalFolder;
+                    auto copyTask = modelFile->CopyAsync(localFolder, modelFileName, NameCollisionOption::ReplaceExisting);
+                    return concurrency::create_task(copyTask).then([](concurrency::task<StorageFile^> modelFileTask2) {
+                        auto path = modelFileTask2.get()->Path;
+                        return path;
+                    });
+                }
+                catch (...)
+                {
+                    Assert::Fail(L"File 01_OneHidden.model must exist in the Documents directory");
+                    throw;
+                }
+            });
+        }
+
+        template<typename Func>
+        void RunTestWithModel(const wchar_t* modelFile, const Func& func)
+        {
+            Platform::String^ modelFileName = ref new Platform::String(modelFile);
+            try {
+                concurrency::create_task(GetModelFilePath(modelFileName)).then([&func](concurrency::task<Platform::String^> modelFilePath) {
+                    auto path = modelFilePath.get();
+                    func(path->Data());
+                }).get();
+            }
+            catch (...) {
+                Assert::Fail(L"Exception while test execution");
+                throw;
+            }
+        }
+
     public:
         TEST_METHOD(TestSanity)
         {
@@ -20,22 +61,17 @@ namespace UWPEvalTests
 
         TEST_METHOD(TestModelLoad)
         {
-            auto device = DeviceDescriptor::CPUDevice();
-            try
-            {
-                CNTK::Function::Load(L"01_OneHidden", device);
-            }
-            catch (...)
-            {
-                // Note: It uses the model trained by Examples\Image\GettingStarted\01_OneHidden.cntk as example. Instructions
-                // to train the model is described in Examples\Image\GettingStarted\README.md. 
-                Assert::Fail(L"File 01_OneHidden must exist in the APPX directory");
-            }
+            RunTestWithModel(L"01_OneHidden.model", [](auto path) {
+                auto device = DeviceDescriptor::CPUDevice();
+                CNTK::Function::Load(path, device);
+            });
         }
 
         TEST_METHOD(TestEval)
         {
-            MultiThreadsEvaluation(false);
+            RunTestWithModel(L"01_OneHidden.model", [](auto path) {
+                MultiThreadsEvaluation(path, false);
+            });
         }
     };
 }
